6a0c85d3ff60f28b197f6ab964284e08f07d420e
Sierra Clibourne <clibo100@mail.chapman.edu>
2019-05-07 16:27:36 -0700

    fixin

--- Haskell Interpreter/Haskell/src/Interpreter.hs
+++ Haskell Interpreter/Haskell/src/Interpreter.hs
@@ -9,8 +9,6 @@ import PrintCPP
 import Data.Map ( Map )
 import qualified Data.Map as M
 import Control.Monad.State ( MonadState, StateT, get, put, modify, foldM, liftIO, lift )
-import Control.Monad.Loops ( whileM_ )
-
 
 data Value = VInt Integer
            | VDouble Double
