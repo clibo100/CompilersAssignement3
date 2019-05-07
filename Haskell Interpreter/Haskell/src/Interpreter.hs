401c6ad59034e203a0f5f8db1e0e8f1c040ef22a
Sierra Clibourne <clibo100@mail.chapman.edu>
2019-05-07 16:23:44 -0700

    fixin

--- Haskell Interpreter/Haskell/src/Interpreter.hs
+++ Haskell Interpreter/Haskell/src/Interpreter.hs
@@ -9,7 +9,7 @@ import PrintCPP
 import Data.Map ( Map )
 import qualified Data.Map as M
 import Control.Monad.State ( MonadState, StateT, get, put, modify, foldM, liftIO, lift )
-import Control.Monad ( whileM_ )
+import Control.Monad.Loops ( whileM_ )
 
 
 data Value = VInt Integer
