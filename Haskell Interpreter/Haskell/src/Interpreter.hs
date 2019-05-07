f5f4931d925fcb2ac3696b5064cd7d10adccddbc
Sierra Clibourne <clibo100@mail.chapman.edu>
2019-05-07 15:35:14 -0700

    added some exps

--- Haskell Interpreter/Haskell/src/Interpreter.hs
+++ Haskell Interpreter/Haskell/src/Interpreter.hs
@@ -180,15 +180,11 @@ evalStm stm =
 
 evalExp :: Interpreter i => Exp -> i Value
 evalExp ETrue = return VTrue
-{-
-evalExp EFalse = 
--}
+evalExp EFalse = return VFalse
 evalExp (EInt i) = return $ VInt i
-{-
-evalExp (EDouble d) = 
-evalExp (EString _) = 
-evalExp (EId i) = 
--}
+evalExp (EDouble d) = return $ VDouble d
+--evalExp (EString _) = 
+--evalExp (EId i) = 
 evalExp (EApp i exps) = do
     vals <- mapM evalExp exps
     case (i, vals) of
@@ -226,32 +222,40 @@ evalExp (EPIncr e@(EId i)) = do
     updateContext i val'
     return val
 evalExp (EPIncr e) = fail $ "Expected " ++ printTree e ++ " to be an id."
-{-
-evalExp (EPDecr e@(EId i)) = 
-evalExp (EPDecr e) = 
+evalExp (EPDecr e@(EId i)) = do
+	val <- evalExp e
+    val' <- subValue val (VInt 1)
+    updateContext i val'
+    return val
+evalExp (EPDecr e) = fail $ "Expected " ++ printTree e ++ " to be an id."
 evalExp (EIncr e@(EId i)) = 
-evalExp (EIncr e) = 
+	val <- evalExp e
+    val' <- addValue val (VInt 1)
+    updateContext i val'
+    return val'
+evalExp (EIncr e) = fail $ "Expected " ++ printTree e ++ " to be an id."
 evalExp (EDecr e@(EId i)) = 
-evalExp (EDecr e) = 
--}
+	val <- evalExp e
+    val' <- subValue val (VInt 1)
+    updateContext i val'
+    return val'
+evalExp (EDecr e) = ail $ "Expected " ++ printTree e ++ " to be an id."
 evalExp (ETimes e1 e2) = applyFun mulValue e1 e2
-{-
-evalExp (EDiv e1 e2)   = 
-evalExp (EPlus e1 e2)  = 
-evalExp (EMinus e1 e2) = 
-evalExp (ELt e1 e2)    = 
-evalExp (EGt e1 e2)    = 
-evalExp (ELtEq e1 e2)  = 
-evalExp (EGtEq e1 e2)  = 
-evalExp (EEq e1 e2)    =
-evalExp (ENEq e1 e2) =
-evalExp (EAnd e1 e2) = 
-evalExp (EOr e1 e2) = 
-evalExp (EAss (EId i) e) = 
-evalExp (EAss _ _) = 
-evalExp (ETyped e _) = 
--}
-evalExp e = fail $ "Missing case in evalExp." ++ printTree e ++ "\n"
+evalExp (EDiv e1 e2)   = applyFun divValue e1 e2
+evalExp (EPlus e1 e2)  = applyFun addValue e1 e2
+evalExp (EMinus e1 e2) = applyFun subValue e1 e2
+evalExp (ELt e1 e2)    = applyFun ltValue e1 e2
+evalExp (EGt e1 e2)    = applyFun gtValue e1 e2
+--evalExp (ELtEq e1 e2)  = applyFun 
+--evalExp (EGtEq e1 e2)  = 
+--evalExp (EEq e1 e2)    =
+--evalExp (ENEq e1 e2) =
+--evalExp (EAnd e1 e2) = 
+--evalExp (EOr e1 e2) = 
+--evalExp (EAss (EId i) e) = 
+--evalExp (EAss _ _) = 
+--evalExp (ETyped e _) = 
+--evalExp e = fail $ "Missing case in evalExp." ++ printTree e ++ "\n"
 
 
 applyFun :: Interpreter i => (Value -> Value -> i Value) -> Exp -> Exp -> i Value
