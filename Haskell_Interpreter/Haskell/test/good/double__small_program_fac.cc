528850e10e806f5a5cc9a03a20b0820228b9fd72
Sierra Clibourne <clibo100@mail.chapman.edu>
2019-05-08 08:48:38 -0700

    in class fixin

--- Haskell_Interpreter/Haskell/test/good/double__small_program_fac.cc
+++ Haskell_Interpreter/Haskell/test/good/double__small_program_fac.cc
@@ -1,4 +1,5 @@
 int main() {
+  printInt(0);
   printDouble(dfac(10.0));
   return 0 ;
 }
@@ -6,9 +7,16 @@ int main() {
 double dfac(double n)
 {
   double f ;
+  printInt(1);
   if (n == 0.0)
+  {
+    printInt(2);
     f = 1.0;
+  }
   else
+  {
+    printInt (3);
     f = n * dfac(n-1.0);
+  }
   return f ;
 }
