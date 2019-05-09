dc9720a5f71a8fc97e000d7123c29137bed8358e
Sierra Clibourne <clibo100@mail.chapman.edu>
2019-05-08 17:20:04 -0700

    continuing interpreter

--- Haskell_Interpreter/Haskell/test/good/double__small_program_fac.cc
+++ Haskell_Interpreter/Haskell/test/good/double__small_program_fac.cc
@@ -8,14 +8,14 @@ double dfac(double n)
 {
   double f ;
   printInt(1);
-  if (n != 0.0)
+  if (n == 0.0)
   {
-    printDouble(n);
-    dfac(n - 1.0);
+    printInt(2);
   }
   else
   {
-    printInt(2);
+    printDouble(n);
+    dfac(n - 1.0);
   }
   return f ;
 }
